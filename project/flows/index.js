e434a51deac1f624c7929cc3a63efb37c63ab6ae
Ilya Bodnar <bendy20p@gmail.com>
2023-07-29 12:43:40 +0300

    Edit allure system

--- project/flows/index.js
+++ project/flows/index.js
@@ -2,7 +2,7 @@
 const mainFlows = require('./main')
 const tablesFlows = require('./tables')
 const {prettifyCamelCase} = require('sat-utils')
-const {stepAllure} = require('../../lib')
+const {step} = require('../../lib')
 const initFlows = {
   ...mainFlows,
   ...tablesFlows
@@ -12,11 +12,9 @@ Object.keys(initFlows).forEach((flowName) => {
   const prettyName = prettifyCamelCase(flowName);
   const fn = initFlows[flowName];
   initFlows[flowName] = async function(...args) {
+    return step(`I ${prettyName}`, () => fn.call(this, ...args), ...args);
 
-// TODO add logger/reporting system
-    return stepAllure(`I ${prettyName}`, () => {});
-    console.log(`I ${prettyName}`);
-    return fn.call(this, ...args)
+
 }
 })
 
